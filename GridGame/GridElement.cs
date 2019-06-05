46d14b5efef46d9fbf8f3e8f771180dca83408a6
Chef <david.penca111@gmail.com>
2019-06-05 20:47:09 +0200

    Commit

---
+++ GridGame/GridElement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace GridGame
+{
+    public class GridElement
+    {
+        int id;
+        int column;
+        int row;
+        int objectType;
+        System.Windows.Controls.Image test;
+        bool initialized;
+
+        public GridElement(int id, int column, int row, int objectType)
+        {
+            this.id = id;
+            this.column = column;
+            this.row = row;
+            this.objectType = objectType;
+            this.test = new System.Windows.Controls.Image();
+            this.initialized = false;
+        }
+
+        public bool checkCollision(GridElement e)
+        {
+            return this.column == e.column && this.row == e.row;
+        }
+
+        public int getColumn()
+        {
+            return this.column;
+        }
+
+        public int getRow()
+        {
+            return this.row;
+        }
+
+        public int getId()
+        {
+            return this.id;
+        }
+
+    public void addCol(int toAdd)
+        {
+            this.column = Math.Max(Math.Min(this.column + toAdd, 9), 0);
+        }
+
+        public void addRow(int toAdd)
+        {
+            this.row = Math.Max(Math.Min(this.row + toAdd, 9), 0);
+        }
+
+        public void draw(System.Windows.Controls.Grid MainGrid)
+        {
+            if (!this.initialized)
+            {
+                this.initialized = true;
+                String imagePath = "./car.png";
+                if (objectType == 0)
+                    imagePath = "./bomb.png";
+
+                this.test = new System.Windows.Controls.Image();
+                BitmapImage bmp = new BitmapImage(new Uri(imagePath, UriKind.Relative));
+
+                test.Source = bmp;
+                MainGrid.Children.Add(test);
+            }
+            
+            Grid.SetColumn(test, column);
+            Grid.SetRow(test, row);
+        }
+
+        public void remove(System.Windows.Controls.Grid MainGrid)
+        {
+            MainGrid.Children.Remove(test);
+        }
+    }
+}
---
+++ GridGame/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GridGame
+{
+    public partial class MainWindow : Window
+    {
+        private GridElement[] bombs;    //Array which holds all the bomb objects
+        private GridElement player;     //Car object
+        private Random r = new Random();
+        long milis;
+        int id = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            File.AppendAllText(@"Logs.txt", "New game:\n");
+
+            StartGame();
+
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyHandler);
+        }
+
+        private void StartGame()
+        {
+            removeOld();
+
+            milis = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            id++;
+            bombs = new GridElement[15];
+            player = new GridElement(0, 0, r.Next(0, 10), 1);
+
+
+            // Setting location of the bombs
+            for (var i = 0; i < bombs.Length; i++)
+            {
+                bombs[i] = new GridElement(i+1, r.Next(1, 10), r.Next(0, 10), 0);
+            }
+
+            drawBombs();
+            player.draw(MainGrid);  // Draws the player car
+        }
+
+        // Draws all bombs in the array
+        private void drawBombs()
+        {
+            if (bombs == null)
+                return;
+
+            foreach (GridElement x in bombs)
+            {
+                x.draw(MainGrid);
+            }
+        }
+
+        // Removes old player and bombs from screen
+        private void removeOld()
+        {
+            if (bombs != null)
+            {
+                foreach (GridElement x in bombs)
+                {
+                    x.remove(MainGrid);
+                }
+            }
+
+            if (player != null)
+                player.remove(MainGrid);
+        }
+
+        // Handles keyboard presses
+        private void MainWindow_KeyHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W || e.Key == Key.Up)
+                player.addRow(-1);
+
+            else if (e.Key == Key.S || e.Key == Key.Down)
+                player.addRow(1);
+
+            else if (e.Key == Key.D || e.Key == Key.Right)
+                player.addCol(1);
+
+            else if (e.Key == Key.A || e.Key == Key.Left)
+                player.addCol(-1);
+
+            player.draw(MainGrid);
+
+            checkEndCondition();
+        }
+
+        // Check if game is over
+        private void checkEndCondition()
+        {
+            foreach (GridElement x in bombs)
+            {
+                if (x.checkCollision(player))
+                {
+                    long time = DateTimeOffset.Now.ToUnixTimeMilliseconds() - milis;
+                    File.AppendAllText(@"Logs.txt", id.ToString() + "|0|" + time.ToString() + "|" + x.getId() + "\n");
+                    StartGame();
+                }
+            }
+
+            if (player.getColumn() == 9)
+            {
+                long time = DateTimeOffset.Now.ToUnixTimeMilliseconds() - milis;
+                File.AppendAllText(@"Logs.txt", id.ToString() + "|1|" + time.ToString() + "|-1\n");
+                StartGame();
+            }
+        }
+
+    }
+}
